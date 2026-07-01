e1daa395119a586f90f2bd7d44e185adbd43c1e2
Pavan Kumar <impavan26Sep@gmail.com>
2026-07-01 20:29:39 +0530

    change-2

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,11 +8,10 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-     origin: [
-          
-          'https://foodify-mu-indol.vercel.app'
-    ],
-    credentials: true
+  origin: 'https://foodify-mu-indol.vercel.app', // Allow only your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // Enable this if you are using cookies/sessions
 }));
 app.use(cookieParser());
 app.use(express.json());
